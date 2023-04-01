06938036e8054cf7dde1d6c2faa2ebdb3bf8e746
satishk38 <64713121+satishk38@users.noreply.github.com>
2023-04-01 17:04:57 -0400

    Update StreamEx.java
    second Github changes

--- src/main/java/org/example/StreamEx.java
+++ src/main/java/org/example/StreamEx.java
@@ -5,7 +5,7 @@ public class StreamEx {
     public static  void main(String []args){
 
         System.out.println("Hi this is my first Github");
-
+System.out.println("this is ritish not parnika");
     }
 
 }
