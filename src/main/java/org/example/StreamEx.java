5d20dbe6bf01e17007a40af938fe4d7c59280a49
satishk38 <64713121+satishk38@users.noreply.github.com>
2023-04-02 07:37:36 -0400

    Update StreamEx.java

--- src/main/java/org/example/StreamEx.java
+++ src/main/java/org/example/StreamEx.java
@@ -7,6 +7,7 @@ public class StreamEx {
         System.out.println("Hi this is my first Github");
         System.out.println("this is ritish not parnika");
         System.out.println("We re in NC and going to DDE");
+          System.out.println("We re in NJ and going to JM");
     }
 
 }
